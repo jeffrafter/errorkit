541b9fe6a4bc7ec611b435fba623be576aaf6d70
Jeff Rafter <jeffrafter@gmail.com>
2013-11-24 11:34:55 -0800

    Fix up the resolved_at spec, passing specs

--- lib/generators/errorkit/templates/app/models/error.rb
+++ lib/generators/errorkit/templates/app/models/error.rb
@@ -2,6 +2,8 @@ class Error < ActiveRecord::Base
   scope :unresolved, -> { where("resolved_at IS NULL") }
 
   def resolve!
+    return true if self.resolved_at.present?
+
     self.update_attribute(:resolved_at, Time.now)
   end
 end
--- lib/generators/errorkit/templates/spec/models/error_spec.rb
+++ lib/generators/errorkit/templates/spec/models/error_spec.rb
@@ -6,12 +6,12 @@ describe Error do
   it "resolves errors" do
     now = Time.now
     Time.stub(:now).and_return(now)
-    Error.should_receive(:update_attribute).with(:resolved_at, now)
+    error.should_receive(:update_attribute).with(:resolved_at, now)
     error.resolve!
   end
 
   it "does not resolve already resolved errors" do
-    Error.should_not_receive(:update_attribute)
+    error.should_not_receive(:update_attribute)
     error.resolved_at = Time.now
     error.resolve!
   end
